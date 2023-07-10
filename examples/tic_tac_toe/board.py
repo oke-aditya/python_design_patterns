5146804a366e99002efefddcc861c7c7be8dee91
Oke Aditya <okeaditya315@gmail.com>
2023-07-11 01:17:36 +0530

    wrap up tic tac toe

--- examples/tic_tac_toe/board.py
+++ examples/tic_tac_toe/board.py
@@ -1,28 +1,35 @@
 from tic_tac_toe.piece_type import PieceType
+from typing import Optional
 
 class TicTacToeBoard:
 
-    def __init__(self, size: int, pieces: list[list[PieceType]]) -> None:
+    def __init__(self, size: int, pieces: Optional[list[list[PieceType]]] = None) -> None:
         self.size = size
-        self.pieces = pieces
+        if pieces is None:
+            self.pieces = self._initialize_empty()
+        else:
+            self.pieces = pieces
 
     
     def add_piece(self, row: int, col: int, piece: PieceType) -> bool:
-        if(self.pieces[row][col] is not None):
+        if(self.pieces[row][col]):
             return False
         else:
             self.pieces[row][col] = piece
             return True
 
     # Create an empty board
-    @staticmethod
-    def initialize_empty(n_rows: int, n_cols: int) -> list[list[PieceType]]:
-        board = [[None] * n_rows] * n_cols
-        return board
+    def _initialize_empty(self) -> list[list[PieceType]]:
+        pieces = [[0] * self.size for i in range(self.size)]
+        return pieces
     
-    def print_board(self,) -> None:
+    def print_board(self) -> None:
         for i in range(self.size):
             for j in range(self.size):
-                print(self.pieces[i][j], sep='\t', end='')
+                print(f"{self.pieces[i][j]} ", end='')
             print()
 
+
+    # Check if anyone has n in row / diagonal / column
+    def _check_winner(self) -> bool:
+        pass
