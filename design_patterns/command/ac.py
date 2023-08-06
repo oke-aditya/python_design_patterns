dde4eb9b13e0ed18b2d2b8e4c91d4dee7f20e762
Oke Aditya <okeaditya315@gmail.com>
2023-08-06 22:22:36 +0530

    Add command design pattern

--- design_patterns/command/ac.py
+++ design_patterns/command/ac.py
@@ -2,16 +2,16 @@ class AirConditioner:
     _is_on: bool = False
     _temperature: int = 25
     
-    def turn_off(self):
+    def turn_off(self) -> None:
         self._is_on = False
         print(f"AC is turned off")
     
-    def turn_on(self):
+    def turn_on(self) -> None:
         self._is_on = True
         print(f"AC is turned on")
     
 
-    def set_temperatue(self, temperature: int):
+    def set_temperatue(self, temperature: int) -> None:
         self._temperature = temperature
         print(f"Temperatue of AC set to {temperature}")
 
