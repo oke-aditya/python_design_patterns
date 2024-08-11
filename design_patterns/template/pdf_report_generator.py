d5a276ca0bfb6c455810ff713a3d36118b168c4b
Oke Aditya <okeaditya315@gmail.com>
2024-08-11 19:57:00 +0530

    Add init to all

---
+++ design_patterns/template/pdf_report_generator.py
@@ -0,0 +1,12 @@
+from template_report_generator import TemplateReportGenerator
+
+class PDFReportGenerator(TemplateReportGenerator):
+
+    def add_header(self) -> None:
+        pass
+
+    def add_content(self) -> None:
+        pass
+
+    def add_footer(self) -> None:
+        pass
